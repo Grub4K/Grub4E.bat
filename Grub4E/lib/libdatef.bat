053787a03a3f1f2d7219057abf4dea48854bf1b1
Simon Sawicki <37424085+Grub4K@users.noreply.github.com>
2021-10-11 00:12:48 +0200

    libdatef: Implement fallback on ambiguous guess

--- Grub4E/lib/libdatef.bat
+++ Grub4E/lib/libdatef.bat
@@ -1,5 +1,6 @@
 @echo off
 setlocal enableDelayedExpansion
+set "returnValue=0"
 
 for /F "tokens=2 delims==." %%a in (
     'wmic os get LocalDateTime /value'
@@ -9,40 +10,57 @@ set "datef.Month=!datef.Date:~4,2!"
 set "datef.Day=!datef.Date:~6,2!"
 set "datef.Date="
 
+set "datef.Once="
+if "!datef.Month!"=="!datef.Day!" (
+    set "datef.Once=1"
+    set "returnValue=1"
+)
+
+set "datef.Lookup= "
+
 :: TODO make this produce unique tokens
 for /F "tokens=1-5 delims=:.,-\_/ " %%e in ("!date!") do (
-    for %%a in (Year Month Day) do (
-        if "%%e"=="!datef.%%a!" set "datef.%%a=e"
-        if "%%f"=="!datef.%%a!" set "datef.%%a=f"
-        if "%%g"=="!datef.%%a!" set "datef.%%a=g"
-        if "%%h"=="!datef.%%a!" set "datef.%%a=h"
-        if "%%i"=="!datef.%%a!" set "datef.%%a=i"
+    for %%b in ("%%e e" "%%f f" "%%g g" "%%h h" "%%k k") do (
+        for /F "tokens=1,2" %%c in ("%%~b") do (
+            for %%a in (Year Day Month) do (
+                if "%%c"=="!datef.%%a!" (
+                    if "!datef.Lookup:%%d=!"=="!datef.Lookup!" (
+                        set "datef.%%a=%%d"
+                        set "datef.Lookup=!datef.Lookup!%%d"
+                    )
+                )
+            )
+        )
     )
 )
 
-set @datef.parseDateTime=for /F "tokens=1-9 delims=:.,-\_/ " %%a in ("^!time: =0^! ^!date^!") do
 
-set "returnValue=0"
 for %%a in (Year Month Day) do (
     if not defined datef.%%a set "returnValue=2"
 )
+
 if !returnValue! equ 2 (
     set @datef.parseDateTime=for /F "tokens=2 delims==+" %%a in ^('wmic os get LocalDateTime /value'^) do set "datef.Date=%%a"^^^&
-    %"
     set "datef.Year=^!datef.Date:~0,-17^!"
-    set "dtf.Month=^!datef.Date:~-17,2^!"
+    set "datef.Month=^!datef.Date:~-17,2^!"
     set "datef.Day=^!datef.Date:~-15,2^!"
     set "datef.Hours=^!datef.Date:~-13,2^!"
     set "datef.Minutes=^!datef.Date:~-11,2^!"
     set "datef.Seconds=^!datef.Date:~-9,2^!"
     set "datef.CentiSeconds=^!datef.Date:~-6,2^!"
 ) else (
-    if "!datef.Month!"=="!datef.Day!" (
-        set @datef.parseDateTime=!@parseDateTime! if "!datef.Month!"=="!datef.Day!"
-        set "returnValue=1"
-    )
-    for %%a in (Year Month Day) do (
-        set "datef.%%a=%%!datef.%%a!"
+    for %%a in (Year Month Day) do set "datef.%%a=%%!datef.%%a!"
+    set @datef.parseDateTime=for /F "tokens=1-9 delims=:.,-\_/ " %%a in ^("^!time: =0^! ^!date^!"^) do
+    if defined datef.Once (
+        set @datef.parseDateTime=!@datef.parseDateTime! ^(if defined datef.Once ^(if 1!datef.Month! GTR 1!datef.Day! ^(^
+                set "datef.Swap=1" ^^^&^
+                set "datef.Once=" ^
+            ^)^)^)^^^&^(^
+                if defined datef.Swap ^(set "datef.Format=!datef.Day! !datef.Month!"^) else set "datef.Format=!datef.Month! !datef.Day!"^
+            ^)^^^&^
+            for /F "tokens=1-2" %%l in ^("^^^!datef.Format^^^!"^) do
+        set "datef.Month=%%l"
+        set "datef.Day=%%m"
     )
     set "datef.Hours=%%a"
     set "datef.Minutes=%%b"
@@ -59,6 +77,9 @@ if !returnValue! equ 2 (
     set "datef.Minutes=%datef.Minutes%"
     set "datef.Seconds=%datef.Seconds%"
     set "datef.CentiSeconds=%datef.CentiSeconds%"
+    set "datef.Format=%datef.Format%"
     set "@datef.parseDateTime=%@datef.parseDateTime%"
+    set "datef.Once=%datef.Once%"
+    set "datef.Swap="
     exit /B %returnValue%
 )
